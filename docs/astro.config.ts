b436157052a9a2024cb2250e33dccf1d1492d236
benji <benjnib09@gmail.com>
2026-04-07 21:38:09 +0100

    feat(docs): add installation to contents pg

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -45,6 +45,7 @@ export default defineConfig({
           label: "Getting Started",
           items: [
             { label: "Introduction", slug: "introduction" },
+            {label: "Installation", slug: "installation" },
           ],
         },
       ],
